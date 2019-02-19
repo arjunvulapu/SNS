cf5d9519fe90b6efc6d3b07422cde1c6417a5717
Apple <apple@Apples-MacBook-Pro.local>
2019-02-19 15:52:01 +0530

    Initial Commit

---
+++ SNS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SNS
+//
+//  Created by Apple on 19/02/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
